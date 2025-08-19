b3d84e9706cae1e3a5dc13d9dea250266088c317
Nagendra219 <76140851+Nagendra219@users.noreply.github.com>
2025-08-19 10:32:36 +0530

    VitalsCheckerTest.java

--- src/test/java/vitals/VitalsCheckerTest.java
+++ src/test/java/vitals/VitalsCheckerTest.java
@@ -10,14 +10,15 @@ public class VitalsCheckerTest {
    @Test
   public void testNotOkWhenAny() throws InterruptedException {
    // English alert
-       assertFalse( VitalsChecker.vitalsOk(94.5f, 102f, 85f, Locale.ENGLISH));
+       assertTrue( VitalsChecker.vitalsOk(94.5f, 102f, 85f, Locale.ENGLISH));
 
         // Spanish alert
-        assertFalse(VitalsChecker.vitalsOk(94.5f, 102f, 85f, new Locale("es"));
+        assertTrue(VitalsChecker.vitalsOk(94.5f, 102f, 85f, new Locale("es"));
 
         // French alert (assuming messages_fr.properties present)
-        assertFalse(VitalsChecker.vitalsOk(94.5f, 102f, 85f, Locale.FRENCH));
+        assertTrue(VitalsChecker.vitalsOk(94.5f, 102f, 85f, Locale.FRENCH));
   }
 }
+
 
 
