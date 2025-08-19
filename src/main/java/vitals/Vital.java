9f2075d25871b1e7456459231ad3c826da5b837f
Nagendra219 <76140851+Nagendra219@users.noreply.github.com>
2025-08-19 10:12:49 +0530

    Vital.java

--- src/main/java/vitals/Vital.java
+++ src/main/java/vitals/Vital.java
@@ -1,12 +1,12 @@
 
 private static class Vital {
-        private final String name;
+         private final String messageKey;
         private final float min;
         private final float max;
         private final String unit;
 
-        Vital(String name, float min, float max, String unit) {
-            this.name = name;
+        Vital(String messageKey, float min, float max, String unit) {
+            this.messageKey = messageKey;
             this.min = min;
             this.max = max;
             this.unit = unit;
@@ -14,9 +14,5 @@ private static class Vital {
 
         boolean isNormal(float value) {
             return value >= min && value <= max;
-        }
-
-        String getAlert(float value) {
-            return name + " out of range (" + value + " " + unit + ").";
         }
     }
